a6dd08871a42d28c42d4e4975d7d5cbdf2a42350
Hakchin Kim <sn2ro@eerfmai.local>
2016-08-21 20:12:11 +0900

    abc

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lblCurrentTime: UILabel!
+    @IBOutlet weak var lblPickerTime: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func dpkDatePicker(sender: UIDatePicker) {
+    }
 
 }
 
