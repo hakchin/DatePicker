4cd0f554a4cf230a68367fa0f419c9d060911b66
Hakchin Kim <sn2ro@eerfmai.local>
2016-08-21 21:49:03 +0900

    uyuyu

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
     @IBOutlet weak var lblCurrentTime: UILabel!
-    @IBOutlet weak var lblPickerTime: UILabel!
+    @IBOutlet weak var lblPickerTime:  UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSTimer.scheduledTimerWithTimeInterval(interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +33,13 @@ class ViewController: UIViewController {
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
         lblPickerTime.text = "선택시간: " + formatter.stringFromDate(datePickerView.date)
         
+    }
+    func updateTime() {
+        let date = NSDate()
+        
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시간: " + formatter.stringFromDate(date)
     }
 
 }
